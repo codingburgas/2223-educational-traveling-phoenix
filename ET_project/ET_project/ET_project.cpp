3a43a212152c0ae24b57dc7bb1a1a30b7c258bd5
Zafir Stoyanov <ZPStoyanov20@codingburgas.bg>
2022-11-09 18:00:23 +0200

    Working menu and back arrow.

--- ET_project/ET_project/ET_project.cpp
+++ ET_project/ET_project/ET_project.cpp
@@ -9,8 +9,8 @@ const int SCREEN_HEIGHT = 1080;
 
 bool QuitMin = false;
 bool StartMin = false;
-
 bool playing = true;
+bool OnMap = false;
 
 SDL_Event eventImage;
 
@@ -24,24 +24,31 @@ SDL_Surface* quit = SDL_LoadBMP("./Graphics/ButtonQuit.bmp");
 
 SDL_Surface* start = SDL_LoadBMP("./Graphics/ButtonPlay.bmp");
 
+SDL_Surface* Arrow = SDL_LoadBMP("./Graphics/ArrowNotHovered.bmp");
+
+SDL_Surface* blackCloset1 = SDL_LoadBMP("./Graphics/ButtonPlay.bmp");
+
 SDL_Texture* BackgroundTexture = SDL_CreateTextureFromSurface(renderer, background);
 
 SDL_Texture* QuitTexture = SDL_CreateTextureFromSurface(renderer, quit);
 
 SDL_Texture* StartTexture = SDL_CreateTextureFromSurface(renderer, start);
 
+SDL_Texture* ArrowTexture = SDL_CreateTextureFromSurface(renderer, Arrow);
+
+SDL_Texture* blackCloset1Texture = SDL_CreateTextureFromSurface(renderer, blackCloset1);
+
 void Quiting(SDL_Rect QuitRect)
 {
 	int x, y;
 	SDL_GetMouseState(&x, &y);
 	if (x >= QuitRect.x and x <= QuitRect.w + QuitRect.x and y >= QuitRect.y and y <= QuitRect.h + QuitRect.y)
-	{
+	{ 
 		playing = false;
-		return void();
 	}
 }
 
-void QuitButtonOnHoverCheck(SDL_Rect QuitRect, bool QuitMin)
+void QuitButtonOnHoverCheck(SDL_Rect QuitRect)
 {
 	int x, y;
 	SDL_GetMouseState(&x, &y);
@@ -49,19 +56,28 @@ void QuitButtonOnHoverCheck(SDL_Rect QuitRect, bool QuitMin)
 	{
 		quit = SDL_LoadBMP("./Graphics/ButtonQuitHovered.bmp");
 		QuitTexture = SDL_CreateTextureFromSurface(renderer, quit);
-		return void(QuitMin);
 	}
 	else
 	{
 		quit = SDL_LoadBMP("./Graphics/ButtonQuit.bmp");
 		QuitTexture = SDL_CreateTextureFromSurface(renderer, quit);
-		if (QuitMin == true)
-		{
-			QuitRect.w = QuitRect.w - 20;
-			QuitRect.h = QuitRect.h - 20;
-			QuitMin = false;
-			return void(QuitMin);
-		}
+	}
+}
+
+
+void ArrowButtonOnHoverCheck(SDL_Rect ArrowRect)
+{
+	int x, y;
+	SDL_GetMouseState(&x, &y);
+	if (x >= ArrowRect.x and x <= ArrowRect.w + ArrowRect.x and y >= ArrowRect.y and y <= ArrowRect.h + ArrowRect.y)
+	{
+		Arrow = SDL_LoadBMP("./Graphics/ArrowHovered.bmp");
+		ArrowTexture = SDL_CreateTextureFromSurface(renderer, Arrow);
+	}
+	else
+	{
+		Arrow = SDL_LoadBMP("./Graphics/ArrowNotHovered.bmp");
+		ArrowTexture = SDL_CreateTextureFromSurface(renderer, Arrow);
 	}
 }
 
@@ -73,12 +89,14 @@ void PlayButtonOnHoverCheck(SDL_Rect StartRect, bool StartMin)
 	{
 		start = SDL_LoadBMP("./Graphics/ButtonPlayHovered.bmp");
 		StartTexture = SDL_CreateTextureFromSurface(renderer, start);
+		
 		return void();
 	}
 	else
 	{
 		start = SDL_LoadBMP("./Graphics/ButtonPlay.bmp");
 		StartTexture = SDL_CreateTextureFromSurface(renderer, start);
+		
 		if (StartMin == true)
 		{
 			StartRect.w = StartRect.w - 20;
@@ -111,31 +129,46 @@ void PlayButtonEvent(SDL_Rect StartRect)
 	SDL_GetMouseState(&x, &y);
 	if (x >= StartRect.x and x <= StartRect.w + StartRect.x and y >= StartRect.y and y <= StartRect.h + StartRect.y)
 	{
+		
+		OnMap = true;
 		background = SDL_LoadBMP("./Graphics/mode_1.bmp");
 		BackgroundTexture = SDL_CreateTextureFromSurface(renderer, background);
 	}
 }
+	
+void BackToMenu(SDL_Rect ArrowRect)
+{
+	int x, y;
+	SDL_GetMouseState(&x, &y);
+	if (x >= ArrowRect.x and x <= ArrowRect.w + ArrowRect.x and y >= ArrowRect.y and y <= ArrowRect.h + ArrowRect.y)
+	{
+		
+		OnMap = false;
+		background = SDL_LoadBMP("./Graphics/background.bmp");
+		BackgroundTexture = SDL_CreateTextureFromSurface(renderer, background);
+	}
+}
 
-void ButtonEvents(SDL_Rect QuitRect, SDL_Rect StartRect, SDL_Rect background, bool QuitMin, bool StartMin)
+void ButtonEvents(SDL_Rect QuitRect, SDL_Rect StartRect, SDL_Rect background, SDL_Rect blackCloset1rect, SDL_Rect ArrowRect, bool QuitMin, bool StartMin)
 {
 	if (SDL_PollEvent(&eventImage))
 	{
-		if (SDL_MOUSEMOTION == eventImage.type)
-		{
-			QuitButtonOnHoverCheck(QuitRect, QuitMin);
-			PlayButtonOnHoverCheck(StartRect, StartMin);
-		}
 		if (SDL_MOUSEBUTTONUP == eventImage.type)
 		{
 			Quiting(QuitRect);
 			PlayButtonEvent(StartRect);
+			BackToMenu(ArrowRect);
+		}
+		if (SDL_MOUSEMOTION == eventImage.type)
+		{
+			QuitButtonOnHoverCheck(QuitRect);
+			PlayButtonOnHoverCheck(StartRect, StartMin);
+			ArrowButtonOnHoverCheck(ArrowRect);
 		}
 		if (SDL_MOUSEBUTTONDOWN == eventImage.type)
 		{
 			QuitButtonEnlarge(QuitRect, QuitMin);
 		}
-
-
 		if (SDL_MOUSEBUTTONDOWN == eventImage.type)
 		{
 			int x, y;
@@ -150,14 +183,13 @@ void ButtonEvents(SDL_Rect QuitRect, SDL_Rect StartRect, SDL_Rect background, bo
 				}
 			}
 		}
-
 	}
 }
 
 int main(int argc, char* args[])
 {
 	SDL_FreeSurface(background);
-	SDL_FreeSurface(quit);
+	SDL_FreeSurface(quit); 
 	SDL_FreeSurface(start);
 
 	SDL_Rect BackRect;
@@ -170,7 +202,7 @@ int main(int argc, char* args[])
 	QuitRect.w = 215;
 	QuitRect.h = 129;
 	QuitRect.x = SCREEN_WIDTH / 2 - QuitRect.w / 2;
-	QuitRect.y = SCREEN_HEIGHT - SCREEN_HEIGHT / 2 + 200;
+	QuitRect.y = SCREEN_HEIGHT - SCREEN_HEIGHT / 2 + 200 - SCREEN_HEIGHT;
 
 	SDL_Rect MouseRect;
 	MouseRect.x = 0;
@@ -182,12 +214,28 @@ int main(int argc, char* args[])
 	StartRect.w = 215;
 	StartRect.h = 129;
 	StartRect.x = SCREEN_WIDTH / 2 - StartRect.w / 2;
-	StartRect.y = SCREEN_HEIGHT / 2;
+	StartRect.y = SCREEN_HEIGHT / 2 - SCREEN_HEIGHT;
 
-	SDL_SetWindowFullscreen(window, SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN ? 0 : SDL_WINDOW_FULLSCREEN);
+	SDL_Rect ArrowRect;
+	ArrowRect.w = 215;
+	ArrowRect.h = 129;
+	ArrowRect.x = 0;
+	ArrowRect.y = 0 - SCREEN_HEIGHT;
+
+	SDL_Rect blackCloset1rect;
+	blackCloset1rect.w = SCREEN_WIDTH / 2;
+	blackCloset1rect.h = SCREEN_HEIGHT;
+	blackCloset1rect.x = 0 - SCREEN_WIDTH / 2;
+	blackCloset1rect.y = 0;
+
+	const int StartY = StartRect.y;
+	const int QuitY = QuitRect.y;
+	const int ArrowY = ArrowRect.y;
+
 	while (playing == true)
 	{
-		ButtonEvents(QuitRect, StartRect, BackRect, QuitMin, StartMin);
+		
+		ButtonEvents(QuitRect, StartRect, BackRect, blackCloset1rect, ArrowRect, QuitMin, StartMin);
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0xff, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear(renderer);
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
@@ -195,7 +243,23 @@ int main(int argc, char* args[])
 		SDL_RenderCopy(renderer, BackgroundTexture, NULL, &BackRect);
 		SDL_RenderCopy(renderer, QuitTexture, NULL, &QuitRect);
 		SDL_RenderCopy(renderer, StartTexture, NULL, &StartRect);
+		SDL_RenderCopy(renderer, ArrowTexture, NULL, &ArrowRect);
+		SDL_RenderCopy(renderer, blackCloset1Texture, NULL, &blackCloset1rect);
 		SDL_RenderPresent(renderer);
+		if (OnMap == true)
+		{	
+			StartRect.y = StartY - SCREEN_HEIGHT;
+			QuitRect.y = QuitY - SCREEN_HEIGHT;
+			ArrowRect.y = ArrowY + SCREEN_HEIGHT;
+		}
+		else
+		{
+			QuitRect.y = QuitY + SCREEN_HEIGHT;
+			StartRect.y = StartY + SCREEN_HEIGHT;
+			ArrowRect.y = ArrowY - SCREEN_HEIGHT;
+		}
+		std::cout << StartRect.y;
+		SDL_Delay(10);
 	}
 	return 0;
 }
